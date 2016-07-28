43217d4bf718a5ef7cd375e9dc1302264020ec6b
marudor <marudor@marudor.de>
2016-07-29 00:09:01 +0200

    typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@ OLD="$(pwd)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 git submodule update --init
-source $DIR/_bootstrapVim.zsh
-source $DIR/_bootstrapZsh.zsh
+source $DIR/_bootstrapVim.sh
+source $DIR/_bootstrapZsh.sh
 cd $OLD
