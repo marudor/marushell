c20bf842f6804887507447b90e1d9e84ef7af636
marudor <marudor@marudor.de>
2016-07-29 00:15:11 +0200

    ensure submodule init on bootstrapVim

--- _bootstrapVim.sh
+++ _bootstrapVim.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 #VIM
+OLD="$(pwd)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR git submodule update --init
+cd $OLD
 ln -s $DIR/vim $HOME/.vim
 ln -s $DIR/vimrc $HOME/.vimrc
 
