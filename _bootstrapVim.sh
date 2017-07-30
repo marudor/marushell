b871f5468a5da268c17b43a1b64327744f9a32ff
marudor <marudor@marudor.de>
2017-07-30 18:24:19 +0200

    better vim bootstrap§

--- _bootstrapVim.sh
+++ _bootstrapVim.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+
 #VIM
 OLD="$(pwd)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR git submodule update --init
 cd $OLD
+rm -rf $HOME/.vim
+rm $HOME/.vimrc
 ln -s $DIR/vim $HOME/.vim
 ln -s $DIR/vimrc $HOME/.vimrc
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim +PluginInstall +qall
