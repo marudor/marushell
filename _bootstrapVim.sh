c64c5962b1b41bd9ade11ec6471cf3dd899d9095
marudor <marudor@marudor.de>
2018-11-27 10:41:37 +0100

    fix mtr setuid

--- _bootstrapVim.sh
+++ _bootstrapVim.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 #VIM
-OLD="$(pwd)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir $DIR/vim
-rm -rf $HOME/.vim
-rm $HOME/.vimrc
-ln -s $DIR/vim $HOME/.vim
-ln -s $DIR/vimrc $HOME/.vimrc
+mkdir "$DIR/vim"
+rm -rf "$HOME/.vim"
+rm "$HOME/.vimrc"
+ln -s "$DIR/vim" "$HOME/.vim"
+ln -s "$DIR/vimrc" "$HOME/.vimrc"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
--- _bootstrapZsh.sh
+++ _bootstrapZsh.sh
@@ -4,17 +4,19 @@ git clone https://github.com/tarjoilija/zgen.git "$HOME/.zgen"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s "$DIR/zshrc" "$HOME/.zshrc"
 
-if type brew 2> /dev/null; then
-  type z 2> /dev/null || brew install fasd
-  type gls 2> /dev/null || brew install coreutils
-  type fzf 2> /dev/null || brew install fzf
-  type pv 2> /dev/null || brew install pv
-  type hub 2> /dev/null || brew install hub
-  if ! type mtr 2> /dev/null; then
+if type brew > /dev/null 2>&1; then
+  type z > /dev/null 2>&1 || brew install fasd
+  type gls > /dev/null 2>&1 || brew install coreutils
+  type fzf > /dev/null 2>&1 || brew install fzf
+  type pv > /dev/null 2>&1 || brew install pv
+  type hub > /dev/null 2>&1 || brew install hub
+  type mtr > /dev/null 2>&1 || brew install mtr
+  if ! type mtr > /dev/null 2>&1; then
     brew install mtr
     mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
-    sudo chmod 4755 "$mtrlocation/sbin/mtr"
-    sudo chown root "$mtrlocation/sbin/mtr"
+    sudo chmod 4755 "$mtrlocation/sbin/mtr-packet"
+    sudo chown root "$mtrlocation/sbin/mtr-packet"
+    sudo chmod u+s "$mtrlocation/sbin/mtr-packet"
   fi
 fi
 
